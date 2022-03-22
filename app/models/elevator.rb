d155b18f9818713f0ff2be7cf408423cdb387a4c
WaelHermassi <waelherm@gmail.com>
2022-03-22 17:35:43 -0400

    Revert "up"
    This reverts commit bef9b82612961ec8d8b6af962528ee669917b5d4.

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -1,10 +1,3 @@
-
 class Elevator < ApplicationRecord
     belongs_to :column
-    after_update do |elevator|
-        if self.status == 'intervention'
-         SmsCreator.new(elevator).send_sms
-        end  
-    end
-end    
-
+end
--- config/initializers/pusher.rb
+++
@@ -1,2 +0,0 @@
-Pusher.app_id = ENV["pusher_app_id"]
-Pusher.key    = ENV["pusher_key"]
