215534960a358813831a32c5d70d675b32db82bb
rolens <rolenstek@gmail.com>
2022-03-23 15:50:40 -0400

    slack api -> removed config token

--- app/services/slack_status.rb
+++ app/services/slack_status.rb
@@ -1,8 +1,21 @@
-
-
+require 'slack-ruby-client'
 
 class SlackStatus
     def initialize(elevator)
         @elevator = elevator
     end
+
+    def send_status
+        Slack.configure do |config|
+            config.token = "no token"
+        end
+        
+        client = Slack::Web::Client.new
+        
+        client.auth_test
+        
+        client.chat_postMessage(channel: '#elevator_operations', text: "Elevator ID: #{@elevator.id} with Serial Number : #{@elevator.serial_number}, changed status from : #{@elevator.status_was} to : #{@elevator.status}.", as_user: true)
+    end
+
+    
 end
