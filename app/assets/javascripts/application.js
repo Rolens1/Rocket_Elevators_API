1b3cee6e997c5c1ca7a0af6493cba94b83ad4ed7
WeverTR <troywever@gmail.com>
2022-03-25 19:59:27 -0400

    fix corrupted repo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,6 +42,40 @@
 //= require chartkick
 //= require Chart.bundle
 
-window.mapsLoaded = function(){
-    console.log(google)
+let map
+
+function initMap(){
+    
+  geocoder = new google.maps.Geocoder()
+
+  var locations = [];
+    //   ['Bondi Beach', -33.890542, 151.274856, 4],
+    //   ['Coogee Beach', -33.923036, 151.259052, 5],
+    //   ['Cronulla Beach', -34.028249, 151.157507, 3],
+    //   ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+    //   ['Maroubra Beach', -33.950198, 151.259302, 1]
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: new google.maps.LatLng(40, -100),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
 }
