94a8d1b5dfb6249c10048e50de4f98a621e96633
troywever@gmail.com <troywever@gmail.com>
2022-03-25 20:50:59 -0400

    push

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,38 +1,38 @@
 #google
-pusher_google_app_key: ENV['pusher_google_app_key']
+Pusher.google_app_key = ENV['Pusher.google_app_key']
 #dropbox
-pusher_dropbox_client_id: ENV['pusher_dropbox_client_id']
-pusher_dropbox_client_secret: ENV['pusher_dropbox_client_secret']
+Pusher.dropbox_client_id = ENV['pusher_dropbox_client_id']
+Pusher.dropbox_client_secret = ENV['pusher_dropbox_client_secret']
 #twilio
-pusher_twilio_app_id: ENV['pusher_twilio_app_id']
-pusher_twilio_key: ENV['pusher_twilio_key']
+Pusher.twilio_app_id = ENV['pusher_twilio_app_id']
+Pusher.twilio_key = ENV['pusher_twilio_key']
 #polly
-pusher_aws_access_key_id: ENV['pusher_aws_access_key_id']
-pusher_aws_secret_access_key: ENV['pusher_aws_secret_access_key']
+Pusher.aws_access_key_id = ENV['pusher_aws_access_key_id']
+Pusher.aws_secret_access_key = ENV['pusher_aws_secret_access_key']
 #sendgrid
-pusher_sendgrid_api: ENV['pusher_sendgrid_api']
+Pusher.sendgrid_api = ENV['pusher_sendgrid_api']
 #freshdesk
-pusher_ticket_uri: ENV['pusher_ticket_uri']
-pusher_ticket_api: ENV['pusher_ticket_api']
+Pusher.ticket_uri = ENV['pusher_ticket_uri']
+Pusher.ticket_api = ENV['pusher_ticket_api']
 #slack
-pusher_slack_api: ENV['pusher_slack_api']
+Pusher.slack_api = ENV['pusher_slack_api']
 
 production:
     #google
-    pusher_google_app_key: ENV['pusher_google_app_key']
+    Pusher.google_app_key = ENV['Pusher.google_app_key']
     #dropbox
-    pusher_dropbox_client_id: ENV['pusher_dropbox_client_id']
-    pusher_dropbox_client_secret: ENV['pusher_dropbox_client_secret']
+    Pusher.dropbox_client_id = ENV['pusher_dropbox_client_id']
+    Pusher.dropbox_client_secret = ENV['pusher_dropbox_client_secret']
     #twilio
-    pusher_twilio_app_id: ENV['pusher_twilio_app_id']
-    pusher_twilio_key: ENV['pusher_twilio_key']
+    Pusher.twilio_app_id = ENV['pusher_twilio_app_id']
+    Pusher.twilio_key = ENV['pusher_twilio_key']
     #polly
-    pusher_aws_access_key_id: ENV['pusher_aws_access_key_id']
-    pusher_aws_secret_access_key: ENV['pusher_aws_secret_access_key']
+    Pusher.aws_access_key_id = ENV['pusher_aws_access_key_id']
+    Pusher.aws_secret_access_key = ENV['pusher_aws_secret_access_key']
     #sendgrid
-    pusher_sendgrid_api: ENV['pusher_sendgrid_api']
+    Pusher.sendgrid_api = ENV['pusher_sendgrid_api']
     #freshdesk
-    pusher_ticket_uri: ENV['pusher_ticket_uri']
-    pusher_ticket_api: ENV['pusher_ticket_api']
+    Pusher.ticket_uri = ENV['pusher_ticket_uri']
+    Pusher.ticket_api = ENV['pusher_ticket_api']
     #slack
-    pusher_slack_api: ENV['pusher_slack_api']
+    Pusher.slack_api = ENV['pusher_slack_api']
